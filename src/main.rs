6399e43007999e548b5df0e2fdde762b542556a6
Martin Kröner <kronerm9@gmail.com>
2023-08-28 12:11:37 +0200

    basic instrument with polyphony working

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         .with_max_level(tracing::Level::TRACE)
         .init();
 
-    let (client, status) =
+    let (client, _status) =
         jack::Client::new("synth-rs", jack::ClientOptions::NO_START_SERVER).unwrap();
 
     let sample_rate = client.sample_rate();
@@ -17,7 +17,10 @@ fn main() {
     let midi_in = client.register_port("midi_in", jack::MidiIn).unwrap();
     let mut audio_out = client.register_port("audio_out", jack::AudioOut).unwrap();
 
-    let mut pressed = false;
+    let frequency_base = 16.35;
+    let step_base = 2f64.powf(1. / 12.);
+
+    let mut keys = [false; 256];
     let active_client = client
         .activate_async(
             (),
@@ -33,29 +36,35 @@ fn main() {
                             midi::MidiMessage::NoteOff {
                                 key_number,
                                 velocity,
-                            } => pressed = false,
+                            } => {
+                                keys[key_number as usize] = false;
+                            }
                             midi::MidiMessage::NoteOn {
                                 key_number,
                                 velocity,
-                            } => pressed = true,
+                            } => {
+                                keys[key_number as usize] = true;
+                            }
                             msg => tracing::warn!("unimplemented midi message: {msg:?}"),
                         }
                     }
 
-                    if pressed {
-                        for v in audio_out.as_mut_slice(ps).iter_mut() {
-                            let x1 = 220. * time * 2. * std::f64::consts::PI;
-                            let y1 = x1.sin();
-                            let x2 = 261.63 * time * 2. * std::f64::consts::PI;
-                            let y2 = x2.sin();
+                    for v in audio_out.as_mut_slice(ps).iter_mut() {
+                        *v = 0.;
+                        for (i, pressed) in keys.iter().enumerate() {
+                            if !pressed {
+                                continue;
+                            }
+                            let freq = frequency_base * step_base.powi(i as i32);
+                            let x = freq * time * 2. * std::f64::consts::PI;
 
-                            let mixed = 0.5 * y1 + 0.7 * y2;
-                            let clipped = mixed.max(-1.).min(1.);
+                            let sine = x.sin();
+                            let square = if sine > 0.5 { 1. } else { 0. };
+                            let sawtooth = (sine + square) / 2.;
 
-                            *v = clipped as f32;
-
-                            time += frame_t;
+                            *v += 0.1 * sine as f32;
                         }
+                        time += frame_t;
                     }
 
                     jack::Control::Continue
@@ -65,26 +74,6 @@ fn main() {
         .unwrap();
 
     // std::thread::sleep_ms(3000);
-
-    tracing::debug!(
-        "{:#?}",
-        active_client.as_client().ports(
-            None,
-            Some(jack::PortSpec::jack_port_type(&jack::AudioIn)),
-            jack::PortFlags::IS_INPUT,
-        )
-    );
-
-    for dst_audio in active_client.as_client().ports(
-        None,
-        Some(jack::jack_sys::FLOAT_MONO_AUDIO),
-        jack::PortFlags::IS_INPUT,
-    ) {
-        active_client
-            .as_client()
-            .connect_ports_by_name("synth-rs:audio_out", &dst_audio)
-            .unwrap();
-    }
 
     tracing::info!("press any key to quit...");
     let mut user_input = String::new();
