6ad7c50900820f484c52d225967284dffd8cad2d
Martin Kröner <gitlab@kroner.dev>
2023-08-30 13:41:42 +0200

    improve oscillator and hertz traits

--- src/hz.rs
+++ src/hz.rs
@@ -1,11 +1,27 @@
-#[derive(Debug, Clone, Copy)]
-pub struct Hertz<T>(T);
+pub trait Hz<T: num::Float> {
+    fn hz(self) -> Hertz<T>;
+}
 
-impl Hertz<f32> {
-    pub fn angular_velocity(self) -> f32 {
-        self.0 * 2. * ::std::f32::consts::PI
+impl<T: num::Float> Hz<T> for T {
+    fn hz(self) -> Hertz<T> {
+        Hertz(self)
     }
 }
+
+impl<T: num::Float> Hz<T> for &T {
+    fn hz(self) -> Hertz<T> {
+        Hertz(*self)
+    }
+}
+
+impl<T: num::Float> Hz<T> for &mut T {
+    fn hz(self) -> Hertz<T> {
+        Hertz(*self)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Hertz<T: num::Float>(T);
 
 impl Hertz<f64> {
     pub fn angular_velocity(self) -> f64 {
@@ -13,50 +29,10 @@ impl Hertz<f64> {
     }
 }
 
-impl<T> std::ops::Deref for Hertz<T> {
+impl<T: num::Float> std::ops::Deref for Hertz<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         &self.0
-    }
-}
-
-pub trait Hz<T> {
-    fn hz(self) -> Hertz<T>;
-}
-
-impl Hz<f32> for f32 {
-    fn hz(self) -> Hertz<f32> {
-        Hertz(self)
-    }
-}
-
-impl Hz<f32> for &f32 {
-    fn hz(self) -> Hertz<f32> {
-        Hertz(*self)
-    }
-}
-
-impl Hz<f32> for &mut f32 {
-    fn hz(self) -> Hertz<f32> {
-        Hertz(*self)
-    }
-}
-
-impl Hz<f64> for f64 {
-    fn hz(self) -> Hertz<f64> {
-        Hertz(self)
-    }
-}
-
-impl Hz<f64> for &f64 {
-    fn hz(self) -> Hertz<f64> {
-        Hertz(*self)
-    }
-}
-
-impl Hz<f64> for &mut f64 {
-    fn hz(self) -> Hertz<f64> {
-        Hertz(*self)
     }
 }
--- src/oscillators.rs
+++ src/oscillators.rs
@@ -1,20 +1,20 @@
 use crate::hz::Hertz;
 
 pub trait Oscillator {
-    fn value(frequency: Hertz<f64>, time: f64) -> f64;
+    fn value(frequency: Hertz<f64>, time: f64) -> f32;
 }
 
 pub struct Sine;
 pub struct Square;
 
 impl Oscillator for Sine {
-    fn value(frequency: Hertz<f64>, time: f64) -> f64 {
-        (frequency.angular_velocity() * time).sin()
+    fn value(frequency: Hertz<f64>, time: f64) -> f32 {
+        (frequency.angular_velocity() * time).sin() as f32
     }
 }
 
 impl Oscillator for Square {
-    fn value(frequency: Hertz<f64>, time: f64) -> f64 {
+    fn value(frequency: Hertz<f64>, time: f64) -> f32 {
         Sine::value(frequency, time).signum()
     }
 }
